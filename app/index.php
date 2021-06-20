36e4109cb8d8ed7b8c422292d3f9c5ca5a2545a9
Carlos Artur <cang.carlos@gmail.com>
2021-06-20 13:09:07 -0300

    feature: LEMP stack working.

--- app/index.php
+++ app/index.php
@@ -8,7 +8,42 @@
 </head>
 
 <body>
-    Pois então tá aí! <?php phpinfo(); ?>
+    CUIDADO! Esse cara é um teste oldstyle, com query na view e tudo o mais kkk
+    <br>
+    ID da execução <?php echo rand(); ?>
+    <hr>
+    <pre>
+        <?php
+        try {
+            $_this = new stdClass();
+            $_this->engine = 'mysql';
+            $_this->host = 'mysql';
+            $_this->database = 'teste';
+            $_this->user = 'root';
+            $_this->pass = 'carlos123';
+            $dns = "{$_this->engine}:port=3306;dbname={$_this->database};host={$_this->host};";
+
+            $connection = new \PDO($dns, $_this->user, $_this->pass);
+            $stmt = $connection->query("SELECT * FROM teste;");
+            while ($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+                print_r($row);
+            }
+        } catch (Exception $exception) {
+            print_r($exception);
+        } catch (Error $error) {
+            print_r($error);
+        }
+        // phpinfo();
+        ?>
+    </pre>
 </body>
 
 </html>
+
+<?php
+function echoHr($value): void
+{
+    echo $value;
+    echo "<hr>";
+}
+?>
